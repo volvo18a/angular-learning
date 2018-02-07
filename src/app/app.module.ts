15941dc499f0c6bab2ca7bc5cf2c9d20bf977a41
volvo18a <475755150@qq.com>
2018-02-07 19:15:28 +0800

    fix a bug about running the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -22,6 +21,6 @@ import { MessageService } from './message.service';
     FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
   providers: [HeroService, MessageService],
-  bootstrap: [AppComponent, HeroesComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
